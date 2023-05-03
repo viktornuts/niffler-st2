805aecc8005fc3211cac5d092b92c52ed1587cec
Сердобинцев Виктор Михайлович (4082021) <vserdobincev@vtb.ru>
2023-05-03 21:29:22 +0300

    Дз 4

--- niffler-e-2-e-tests/src/test/java/niffler/test/SpendsWebTest.java
+++ niffler-e-2-e-tests/src/test/java/niffler/test/SpendsWebTest.java
@@ -1,24 +1,21 @@
 package niffler.test;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
-
 import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import niffler.jupiter.GenerateCategory;
 import niffler.jupiter.GenerateCategoryExtension;
-import niffler.jupiter.GenerateSpend;
-import niffler.jupiter.GenerateSpendExtension;
-import niffler.model.CategoryJson;
-import io.qameta.allure.AllureId;
 import niffler.jupiter.annotation.GenerateSpend;
+import niffler.jupiter.extension.GenerateSpendExtension;
 import niffler.model.CurrencyValues;
 import niffler.model.SpendJson;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 
 
 @ExtendWith(GenerateCategoryExtension.class)
